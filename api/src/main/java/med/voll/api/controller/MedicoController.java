37424daee1b2ebc5feacdcf94c9cbb9d2b3e98b3
Hudson Tadeu <hudinvesting@gmail.com>
2023-04-24 20:20:07 -0300

    Migrations com Flyway

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
@@ -18,6 +19,7 @@ public class MedicoController {
   private MedicoRepository repository;
 
   @PostMapping
+  @Transactional
   public void cadastrar(@RequestBody DadosCadastroMedico dados) {
     repository.save(new Medico(dados));
   }
