b19ac90ae17581e8e61c30b074db01adb37cf831
Hudson Tadeu <hudinvesting@gmail.com>
2023-04-25 14:42:21 -0300

    Nova Migration

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -18,6 +18,7 @@ public class Medico {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    private String telefone;
     private String email;
     private String crm;
 
@@ -30,6 +31,7 @@ public class Medico {
     public Medico(DadosCadastroMedico dados) {
       this.nome = dados.nome();
       this.email = dados.email();
+      this.telefone = dados.telefone();
       this.crm = dados.crm();
       this.especialidade = dados.especialidade();
       this.endereco = new Endereco(dados.endereco());
