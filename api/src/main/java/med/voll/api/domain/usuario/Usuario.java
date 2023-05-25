3fd06793a58353b08241db6d7717c0a6f43e1adb
Hudson Tadeu <hudinvesting@gmail.com>
2023-05-25 15:42:34 -0300

    Controller de autenticação

--- api/src/main/java/med/voll/api/domain/usuario/Usuario.java
+++ api/src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -1,5 +1,13 @@
 package med.voll.api.domain.usuario;
 
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -13,11 +21,46 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 
-public class Usuario {
+public class Usuario implements UserDetails {
 
-  @Id 
+  @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String login;
   private String senha;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return new ArrayList<>(Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+  }
+
+  @Override
+  public String getPassword() {
+    return senha;
+  }
+
+  @Override
+  public String getUsername() {
+    return login;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
