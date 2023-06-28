133e2b4ea904bd00a7e5ce30c45b57d6c56be00c
Hudson Tadeu <hudinvesting@gmail.com>
2023-06-28 15:23:20 -0300

    Testando o controle de acesso

--- api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -11,10 +11,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+
+  @Autowired
+  private SecurityFilter securityFilter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,7 +28,8 @@ public class SecurityConfigurations {
         .and().authorizeHttpRequests()
         .requestMatchers(HttpMethod.POST, "/login").permitAll()
         .anyRequest().authenticated()
-        .and().build();
+        .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+        .build();
   }
 
   @Bean
